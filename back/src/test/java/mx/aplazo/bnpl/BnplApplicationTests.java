7b21e05b21d835c13b1d7e1d5f66e99450a981c7
caiseL <cristophertraperoo@gmail.com>
2025-02-23 12:31:42 -0700

    feat: initial spring boot setup

---
+++ back/src/test/java/mx/aplazo/bnpl/BnplApplicationTests.java
@@ -0,0 +1,13 @@
+package mx.aplazo.bnpl;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BnplApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
