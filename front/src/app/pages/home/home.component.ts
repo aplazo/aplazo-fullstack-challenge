604580a0d53277383d0cf69a63a95ce71ddebff4
caiseL <cristophertraperoo@gmail.com>
2025-02-25 08:39:07 -0700

    feat: implement form to create loans

--- front/src/app/pages/home/home.component.ts
+++ front/src/app/pages/home/home.component.ts
@@ -1,8 +1,84 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import {
+  FormControl,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
+import { AplazoButtonComponent } from '@apz/shared-ui/button';
+import { AplazoLogoComponent } from '@apz/shared-ui/logo';
+import { NgIf } from '@angular/common';
+import { Router } from '@angular/router';
+import { ROUTE_CONFIG } from '../../config/routes.config';
+import { CreateLoanRequest, LoansService } from '../../services/loans.service';
 
 @Component({
   standalone: true,
   selector: 'app-home',
   templateUrl: './home.component.html',
+  imports: [
+    ReactiveFormsModule,
+    AplazoButtonComponent,
+    AplazoLogoComponent,
+    NgIf,
+  ],
 })
-export class HomeComponent {}
+export class HomeComponent implements OnInit {
+  errorMessage: string | null = null;
+
+  constructor(
+    private loansService: LoansService,
+    private router: Router
+  ) {}
+
+  readonly customerId = new FormControl<string>('', {
+    nonNullable: true,
+    // TODO: uuid validator
+    validators: [Validators.required],
+  });
+
+  readonly amount = new FormControl<number>(0, {
+    nonNullable: true,
+    validators: [Validators.required],
+  });
+
+
+  ngOnInit(): void {
+    // TODO: remove this
+    this.form.setValue({
+      customerId: '0ef9e80a-f68c-4a49-95a9-d39626e0acb6',
+      amount: 5,
+    });
+  }
+
+  readonly form = new FormGroup({
+    customerId: this.customerId,
+    amount: this.amount
+  });
+
+  createLoan(): void {
+    console.log('Creating loan:', this.form.value);
+    if (this.form.invalid) {
+      console.log('Invalid form');
+      return;
+    }
+
+    this.errorMessage = null;
+
+    const createLoanRequest: CreateLoanRequest = {
+      customerId: this.customerId.value,
+      amount: this.amount.value,
+    };
+    this.loansService.createLoan(createLoanRequest).subscribe({
+      next: (customer) => {
+        console.log('Loan created:', customer);
+        // this.router.navigate([ROUTE_CONFIG.app, ROUTE_CONFIG.home]);
+      },
+      error: (error) => {
+        this.errorMessage = error.error?.message || 'Failed to create loan';
+        console.log('Error creating loan:', this.errorMessage);
+      },
+    });
+  }
+}
+
---
+++ front/src/app/services/loans.service.ts
@@ -0,0 +1,29 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class LoansService {
+  // TODO: share api url across services
+  private apiUrl = 'http://localhost:8080/v1/loans';
+
+  constructor(private http: HttpClient) {}
+
+  getLoanById(id: number): Observable<LoanResponse> {
+    return this.http.get<LoanResponse>(`${this.apiUrl}/${id}`);
+  }
+
+  createLoan(loan: CreateLoanRequest): Observable<LoanResponse> {
+    // TODO: send jwt token
+    return this.http.post<LoanResponse>(this.apiUrl, loan);
+  }
+}
+
+export interface CreateLoanRequest {
+  customerId: string;
+  amount: number;
+}
+
+export interface LoanResponse {}
